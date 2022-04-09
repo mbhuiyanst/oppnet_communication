06593c63c9a18ad26dc2c87493f3b426e8fd696b
mbhuiyanst <92376890+mbhuiyanst@users.noreply.github.com>
2022-04-10 01:04:54 +0200

    Update mesageController.dart

--- lib/controller/mesageController.dart
+++ lib/controller/mesageController.dart
@@ -1,4 +1,6 @@
 import 'package:oppnet_chat/models/message.dart';
+
+/// Here a controller class is defined , "MessageController" is required for controlling send/recive text and showing them on the chat page for real time communication
 class MessageController{
   List<MessageModel> messages = [];
   final List<MessageModel> _chatMessages = [];
